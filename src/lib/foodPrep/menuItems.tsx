e42d6e799d4f8f2712e9fc694b51005a1724bb91
Brendan <brendanfgr@gmail.com>
2025-04-07 09:42:21 +0100

    Cleaning up the repo by removing unused files and deps

--- src/lib/foodPrep/menuItems.tsx
+++
@@ -1,8 +0,0 @@
-import { BarChart3, Package } from "lucide-react";
-
-// Note that the order count is hard coded at 4
-export const menuItems = [
-    { label: "Dashboard", icon: <BarChart3 className="mr-2 h-5 w-5" />, value: "dashboard", path: "/food-prep-dashboard" },
-    { label: "Orders", icon: <Package className="mr-2 h-5 w-5" />, value: "orders", badgeCount: 4, path: "/food-prep-dashboard/orders" },
-    { label: "Inventory", icon: <Package className="mr-2 h-5 w-5" />, value: "inventory", path: "/food-prep-dashboard/inventory" }
-];
